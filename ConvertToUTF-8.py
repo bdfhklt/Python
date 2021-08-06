45b357f40f536fcaf9c0024e59120d74d6dc1fe6
tmp20190820 <54312936+Park-YongHyun@users.noreply.github.com>
2021-08-06 22:16:53 +0900

    v20210806

---
+++ ConvertToUTF-8.py
@@ -0,0 +1,12 @@
+import os, glob, chardet
+
+if __name__ == "__main__":
+	# 텍스트 파일의 인코딩을 'UTF-8'로 변환
+	for filePath in glob.glob(os.path.join(os.path.dirname(__file__), 'input/*')):
+		fileContent = ''
+		with open(filePath, 'rb') as f: # 인코딩 문제로 바이너리로 열어 인코딩 감지
+			fileEncoding = chardet.detect(f.read())
+		with open(filePath, 'r', encoding=fileEncoding['encoding']) as f:
+			fileContent = f.read()
+		with open(filePath, 'w', encoding='utf-8') as f:
+			f.write(fileContent)
