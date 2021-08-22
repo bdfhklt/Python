2055f024fbaf218439a10f7a93668e1d72c7d257
tmp20190820 <54312936+Park-YongHyun@users.noreply.github.com>
2021-08-22 21:22:16 +0900

    v20210822

--- Gcode test.py
+++ Gcode test.py
@@ -6,6 +6,7 @@ center = 150
 def sin(a): return round(math.sin(math.radians(a)), 3)
 def cos(a): return round(math.cos(math.radians(a)), 3)
 
+# 360도 테스트
 def speedTestXY360(speedMin, speedMax, step):
     gcode = ""
     for s in range(speedMin, speedMax, step):
@@ -24,26 +25,30 @@ def speedTestXY360(speedMin, speedMax, step):
     gcode += "M117 end\n"
     return gcode
 
+# X, Y 개별 테스트
 def speedTestXY(xy, speedMin, speedMax, step):
     gcode = ""
     gcode += "G4 S1\n"
-    for s in range(speedMin, speedMax, step):
-        gcode += "M117 speed {}: {}\n".format(xy, s)
-        m = 0
-        if s < 120:
-            m = s
+    for speed in range(speedMin, speedMax, step):
+        gcode += "M117 speed {}: {}\n".format(xy, speed)
+        distance = 0
+        if speed < 120:
+            distance = speed
         else:
-            m = 120
-        gcode += "G0 F{} {}{}\n".format(s * 60, xy, center + m)
-        gcode += "G0 F{} {}{}\n".format(s * 60, xy, center - m)
-        gcode += "G0 F3000 {}{}\n".format(xy, center)
-        gcode += "G4 P200\n"
+            distance = 120
+        gcode += "G0 F12000 {}{}\n".format(xy, center - distance)
+        gcode += "G4 P100\n"
+        gcode += "G0 F{} {}{}\n".format(speed * 60, xy, center + distance)
+        gcode += "G0 F{} {}{}\n".format(speed * 60, xy, center - distance)
+        gcode += "G4 P100\n"
+    gcode += "G0 F12000 {}{}\n".format(xy, center)
     gcode += "M117 end\n"
     return gcode
 
-gcode += "M204 S1000\nM205 X10 Y10\nG0 F4200 X{0} Y{0} Z1\n".format(center)
-speedMin = 50
-speedMax = 105
+# M204: 가속도, M205: X저크 Y저크
+gcode += "M204 S800\nM205 X8 Y8\nG0 F6000 X{0} Y{0} Z1\n".format(center)
+speedMin = 20
+speedMax = 100 + 1
 step     = 5
 # gcode += speedTestXY360(speedMin, speedMax, step)
 gcode += speedTestXY("X", speedMin, speedMax, step)
